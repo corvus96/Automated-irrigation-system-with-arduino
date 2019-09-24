455ee62c603b5dc50c058eb50e438e0b798b4fd8
Guillermo Raven <guillermoraven.96@hotmail.com>
2019-09-24 00:59:26 -0400

    Revert "A new begin with arduino IDE"
    This reverts commit 1b80e347f416ebeeb1a4e7be576a522d04d56ecf.

--- secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "CONTROLADA"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "*11925976*"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 000000			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "SDJZAGS7POOE8AQI"   // replace XYZ with your channel write API Key
